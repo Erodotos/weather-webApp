703443a0bee2421394dc5de14eb289f94b49e0fc
Erodotos <edemet01@cs.ucy.ac.cy>
2019-03-31 13:59:38 +0300

    added city selection

--- js/main.js
+++ js/main.js
@@ -4,37 +4,49 @@ function selectOnlyThis(id) {
     document.getElementById(id).checked = true;
 }
 
-function checkData(){
+function checkData() {
 
     let address = document.getElementById("address").value;
     let region = document.getElementById("region").value;
     let city = document.getElementById("city").value;
 
-    if ( address === ''){
+    if (address === '' || !address.replace(/\s/g, '').length) {
         document.getElementById("invalid-address").style.visibility = 'visible';
-    }else{
+    } else {
         document.getElementById("invalid-address").style.visibility = 'hidden';
-        // address = true;
     }
-    
-    if (region === ''){
+
+    if (region === '' || !region.replace(/\s/g, '').length) {
         document.getElementById("invalid-region").style.visibility = 'visible';
-    }else{
+    } else {
         document.getElementById("invalid-region").style.visibility = 'hidden';
-        //region = true;
     }
 
-    if (city === ''){
+    if (city === 'Select city' || !city.replace(/\s/g, '').length) {
         document.getElementById("invalid-city").style.visibility = 'visible';
-    }else{
+    } else {
         document.getElementById("invalid-city").style.visibility = 'hidden';
-        //city=true;
     }
 
-    if (!address || !region || !city){
+    if (!address || !region || (city === 'Select city') || !address.replace(/\s/g, '').length || !region.replace(/\s/g, '').length) {
         return;
-    }else{
+    } else {
         alert("We are ready for an ajax call");
     }
-    
+
+}
+
+function resetFields() {
+
+    document.getElementById("address").value = '';
+    document.getElementById("region").value = '';
+    document.getElementById("city").value = 'Select city';
+
+    document.getElementById("invalid-address").style.visibility = 'hidden';
+    document.getElementById("invalid-region").style.visibility = 'hidden';
+    document.getElementById("invalid-city").style.visibility = 'hidden';
+
+    document.getElementById("celsius").checked = true;
+    document.getElementById("farenheit").checked = false;
+
 }
